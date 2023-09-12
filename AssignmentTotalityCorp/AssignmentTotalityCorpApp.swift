c01395fc4fbeaa5afd3fd00d6644465d53560e60
Aryan Jagarwal <ajbaggar@gmail.com>
2023-09-12 19:31:23 +0530

    Initial Commit

---
+++ AssignmentTotalityCorp/AssignmentTotalityCorpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentTotalityCorpApp.swift
+//  AssignmentTotalityCorp
+//
+//  Created by Aryan Jagarwal on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentTotalityCorpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentTotalityCorp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AssignmentTotalityCorp
+//
+//  Created by Aryan Jagarwal on 12/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
